0db7404346695ad776849b142eb175db5b9bb192
Matrix53 <1079207272@qq.com>
2024-06-11 20:08:20 +0800

    chore: add settinhg

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,126 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './api/ipc'
-// If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
+import { db } from './utils/db'
 
+// add timer for web notification
+async function addTodoTimer() {
+  // get todos
+  let now = new Date().getTime()
+  let nowPlusADay = new Date(now + 24 * 60 * 60 * 1000).getTime()
+  let todos = await db.todos
+    .where('remindTime')
+    .between(now, nowPlusADay)
+    .toArray()
+  // set timer for web notification
+  todos.forEach((todo) => {
+    let timer = todo.remindTime! - new Date().getTime()
+    if (timer < 0 || localStorage.getItem(`${todo.id}`)) return
+    let timeoutID = setTimeout(() => {
+      new Notification('您有一个日程需要处理', {
+        body: todo.name,
+        requireInteraction: true,
+        icon: '/icon.ico',
+      })
+      if (localStorage.getItem(`${todo.id}`)) {
+        localStorage.removeItem(`${todo.id}`)
+      }
+    }, timer)
+    // save timeoutID
+    localStorage.setItem(`${todo.id}`, `${timeoutID},${todo.remindTime}`)
+  })
+  // clear outdated notification in localStorge
+  let keys = Object.keys(localStorage)
+  keys.forEach((key) => {
+    let value = localStorage.getItem(key)!.split(',')
+    let timeoutID = parseInt(value[0])
+    let remindTime = parseInt(value[1])
+    if (remindTime < new Date().getTime()) {
+      clearTimeout(timeoutID)
+      localStorage.removeItem(key)
+    }
+  })
+}
+// run addTodoTimer every 23 hours
+addTodoTimer()
+setInterval(addTodoTimer, 23 * 60 * 60 * 1000)
+
+// add hooks to db when update todos
+db.todos.hook('creating', function (primKey, obj, transaction) {
+  // You may do additional database operations using given transaction object.
+  // You may also modify given obj
+  // You may set this.onsuccess = function (primKey){}. Called when autoincremented key is known.
+  // You may set this.onerror = callback if create operation fails.
+  // If returning any value other than undefined, the returned value will be used as primary key
+  this.onsuccess = function (primKey) {
+    if (!obj.remindTime) return
+    let timer = obj.remindTime - new Date().getTime()
+    if (timer < 0) return
+    let timeoutID = setTimeout(() => {
+      new Notification('您有一个日程需要处理', {
+        body: obj.name,
+        requireInteraction: true,
+        icon: '/icon.ico',
+      })
+      if (localStorage.getItem(`${primKey}`)) {
+        localStorage.removeItem(`${primKey}`)
+      }
+    }, timer)
+    localStorage.setItem(`${primKey}`, `${timeoutID},${obj.remindTime}`)
+  }
+})
+db.todos.hook('updating', function (modifications, primKey, obj, transaction) {
+  // You may use transaction to do additional database operations.
+  // You may not do any modifications on any of the given arguments.
+  // You may set this.onsuccess = function (updatedObj){} when update operation completes.
+  // You may set this.onerror = callback if update operation fails.
+  // If you want to make additional modifications,
+  // return another modifications object
+  // containing the additional or overridden modifications to make. Any returned
+  // object will be merged to the given modifications object.
+  this.onsuccess = function (updatedObj) {
+    if (!updatedObj.remindTime) return
+    let timer = updatedObj.remindTime - new Date().getTime()
+    if (timer < 0) return
+    let value = localStorage.getItem(`${primKey}`)
+    if (value) {
+      let timeoutID = parseInt(value.split(',')[0])
+      clearTimeout(timeoutID)
+    }
+    let newTimeoutID = setTimeout(() => {
+      new Notification('您有一个日程需要处理', {
+        body: updatedObj.name,
+        requireInteraction: true,
+        icon: '/icon.ico',
+      })
+      if (localStorage.getItem(`${primKey}`)) {
+        localStorage.removeItem(`${primKey}`)
+      }
+    }, timer)
+    localStorage.setItem(
+      `${primKey}`,
+      `${newTimeoutID},${updatedObj.remindTime}`
+    )
+  }
+})
+db.todos.hook('deleting', function (primKey, obj, transaction) {
+  // You may do additional database operations using given transaction object.
+  // You may set this.onsuccess = callback when delete operation completes.
+  // You may set this.onerror = callback if delete operation fails.
+  // Any modification to obj is ignored.
+  // Any return value is ignored.
+  // throwing exception will make the db operation fail.
+  this.onsuccess = function () {
+    let value = localStorage.getItem(`${primKey}`)
+    if (!value) return
+    let timeoutID = parseInt(value.split(',')[0])
+    clearTimeout(timeoutID)
+    localStorage.removeItem(`${primKey}`)
+  }
+})
+
+// create app
 const app = createApp(App)
 app.use(router)
 app.mount('#app').$nextTick(() => {
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="test"></div>
+  <div class="setting-container">
+    <n-form
+      :style="{
+        maxWidth: '500px',
+      }"
+      :model="formValue"
+    >
+      <n-form-item label="API域名" path="url">
+        <n-input
+          placeholder="例如: https://api.openai.com"
+          v-model:value="formValue.url"
+        />
+      </n-form-item>
+      <n-form-item label="API密钥" path="key">
+        <n-input placeholder="您的API密钥" v-model:value="formValue.key" />
+      </n-form-item>
+    </n-form>
+    <n-button type="primary" @click="handleSave">保存设置</n-button>
+    <n-button
+      :style="{
+        marginLeft: '15px',
+      }"
+      @click="handleCheckUpdate"
+      >检查更新</n-button
+    >
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NForm, NFormItem, NInput, NButton, useMessage } from 'naive-ui'
+import { ref, onBeforeMount } from 'vue'
+
+const formValue = ref({
+  url: '',
+  key: '',
+})
+const message = useMessage()
+
+function handleSave() {
+  localStorage.setItem('api', JSON.stringify(formValue.value))
+  message.success('保存成功')
+}
+function handleCheckUpdate() {
+  console.log('check update')
+}
+
+onBeforeMount(() => {
+  const api = localStorage.getItem('api')
+  if (api) {
+    Object.assign(formValue.value, JSON.parse(api))
+  }
+})
+</script>
 
 <style scoped>
-.test {
-  background-color: black;
-  width: 200px;
-  height: 200px;
+.setting-container {
+  box-sizing: border-box;
+  padding: 20px;
 }
 </style>
